716cfaf9ce2f7f477780c30b305364528ddf6a6a
edge535 <1204565324@qq.com>
2020-05-12 18:55:16 +0800

    静态页面布局

---
+++ web/project/js/js1.js
@@ -0,0 +1,10 @@
+/*菜单显示折叠*/
+function show_menu(){
+	var menu1 = document.getElementById('menu1');
+	menu1.style.display = 'block';
+}
+
+function show_menu1(){
+	var menu1 = document.getElementById('menu1');
+	menu1.style.display = 'none';
+}
